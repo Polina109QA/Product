355d21ef6afcde94647d102797b89ac1d91cb5a1
Polina Pekhota <PPekhota@inno.tech>
2025-10-15 12:01:50 +0300

    Изменена основная логика Добавлен класс коробка

--- src/main/java/org/example/Develop.java
+++ src/main/java/org/example/Develop.java
@@ -4,5 +4,7 @@ public class Develop {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("111111");
+        System.out.println("222");
+        System.out.println("33333");
     }
 }
---
+++ src/main/java/org/example/box.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public box(int size) {
+        this.size = size;
+    }
+
+}
